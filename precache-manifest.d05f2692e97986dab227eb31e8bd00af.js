439a73c3c902e8a7d92de2d87f37f04fb97fc9ac
Rodica Ciorba <rodica.ciorba@ffwagency.com>
2020-05-07 19:38:36 +0300

    Updates

---
+++ precache-manifest.d05f2692e97986dab227eb31e8bd00af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b5bf64ccbe46a04e093f8002d88f09f",
+    "url": "/react-rolodex/index.html"
+  },
+  {
+    "revision": "df7e3281b6ac081696bd",
+    "url": "/react-rolodex/static/css/main.b1b0e731.chunk.css"
+  },
+  {
+    "revision": "2e48362b289cd0b95b27",
+    "url": "/react-rolodex/static/js/2.ac90d78e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-rolodex/static/js/2.ac90d78e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df7e3281b6ac081696bd",
+    "url": "/react-rolodex/static/js/main.6f06e036.chunk.js"
+  },
+  {
+    "revision": "765ec451183e5339a1e5",
+    "url": "/react-rolodex/static/js/runtime-main.e6df90a7.js"
+  }
+]);
